70d36f42cbb2afeb310c96a0a19d7859a7684894
Renzo-Espinola <renzo.espinola87@gmail.com>
2020-09-13 23:55:00 -0300

    41-manejoArchivo

---
+++ 41-ManejoArchivos/src/test/TestManejoArchivos.java
@@ -0,0 +1,12 @@
+package test;
+import static  manejoarchivos.ManejoArchivos.*;
+public class TestManejoArchivos {
+
+    public static void main(String[] args) {
+        var nombreArchivo = "Prueba.txt";
+        //crearArchivo(nombreArchivo);
+        // anexarArchivo(nombreArchivo,"HOLA RENZO");
+       //anexarArchivo(nombreArchivo,"WEEEEEP");
+        leerArchivo(nombreArchivo);
+    }
+}
